6c8d747cf12934bf40e6d480c41a280781a34c60
sabzabi <113032368+sabzabi@users.noreply.github.com>
2023-10-25 23:21:15 +0700

    add session

--- DebtManagement/Program.cs
+++ DebtManagement/Program.cs
@@ -8,7 +8,12 @@ builder.Services.AddRazorPages();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<DebtManagementContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10000);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +36,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
